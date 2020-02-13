7e877332dd1f686bed768b8a6f0696dac5762bb1
Ludivine <ludivine_m@hotmail.com>
2020-02-13 13:48:08 +0100

    Design answers table schema and rename answers column to answers_count in the questions table

--- app/User.php
+++ app/User.php
@@ -46,5 +46,10 @@ class User extends Authenticatable
     {
         // return route("questions.show", $this->id);
         return '#';
-    }    
+    }  
+      
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
