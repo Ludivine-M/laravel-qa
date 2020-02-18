ee0dc86f3512eb08e2f44ac14eb1e93a2288109f
Ludivine <ludivine_m@hotmail.com>
2020-02-18 10:55:59 +0100

    implement DRY principle on Answer and Question models

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+
+    use VotableTrait;
+
     protected $fillable = ['body', 'user_id'];
 
     public function question()
@@ -22,14 +25,14 @@ class Answer extends Model
     {
         return \Parsedown::instance()->text($this->body);
     }
-    
+
     public static function boot()
     {
         parent::boot();
 
         static::created(function ($answer) {
-            $answer->question->increment('answers_count');        
-        });        
+            $answer->question->increment('answers_count');
+        });
 
         static::deleted(function ($answer) {
             $answer->question->decrement('answers_count');
@@ -54,20 +57,5 @@ class Answer extends Model
     public function isBest()
     {
         return $this->id === $this->question->best_answer_id;
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
--- app/Question.php
+++ app/Question.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Str;
 
 class Question extends Model
 {
+    use VotableTrait;
+
     protected $fillable = ['title', 'body'];
 
     public function user() {
@@ -77,20 +79,4 @@ class Question extends Model
     {
         return $this->favorites->count();
     }
-
-  
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
-    }  
 }
