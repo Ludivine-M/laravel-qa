19e99268bee63dd27c9dbe544fa67da5c89af912
Ludivine <ludivine_m@hotmail.com>
2020-02-21 12:59:16 +0100

    create accept answer component

--- resources/views/shared/_accept.blade.php
+++ resources/views/shared/_accept.blade.php
@@ -1,14 +1,14 @@
 @can ('accept', $model)
-<a title="Mark this model as best model" class="{{ $model->status }} mt-2" onclick="event.preventDefault(); document.getElementById('accept-model-{{ $model->id }}').submit();">
-    <i class="fa fa-check fa-2x"></i>
-</a>
-<form id="accept-model-{{ $model->id }}" action="{{ route('answers.accept', $model->id) }}" method="POST" style="display:none;">
-    @csrf
-</form>
+    <a title="Mark this model as best model" class="{{ $model->status }} mt-2" onclick="event.preventDefault(); document.getElementById('accept-model-{{ $model->id }}').submit();">
+        <i class="fa fa-check fa-2x"></i>
+    </a>
+    <form id="accept-model-{{ $model->id }}" action="{{ route('answers.accept', $model->id) }}" method="POST" style="display:none;">
+        @csrf
+    </form>
 @else
-@if ($model->is_best)
-<a title="The question owner accepted this model as best model" class="{{ $model->status }} mt-2">
-    <i class="fas fa-check fa-2x"></i>
-</a>
-@endif
+    @if ($model->is_best)
+        <a title="The question owner accepted this model as best model" class="{{ $model->status }} mt-2">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+    @endif
 @endcan
