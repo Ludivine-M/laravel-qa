d66c93f8f6cd0ec2be0834d4008d110a7f63ff4b
ludivine-m <ludivine_m@hotmail.com>
2020-03-11 14:45:19 +0100

    integrate syntax highlight

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -21,9 +21,11 @@
 
 <script>
 import MarkdownIt from "markdown-it";
+import prism from 'markdown-it-prism'
 import autosize from "autosize";
 
 const md = new MarkdownIt();
+md.use(prism);
 
 export default {
   props: ["body"],
