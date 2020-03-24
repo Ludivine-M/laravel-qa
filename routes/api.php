22cbfd0c952b59c274ea106e990b71383fc92c7d
ludivine-m <ludivine_m@hotmail.com>
2020-03-24 12:19:20 +0100

    Create api endpoints for vote question and answer

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,9 @@ Route::get('/questions/{question}-{slug}', 'Api\QuestionDetailsController');
 Route::middleware(['auth:api'])->group(function() {
     Route::apiResource('/questions', 'Api\QuestionsController')->except('index'); 
     Route::apiResource('/questions.answers', 'Api\AnswersController')->except('index'); 
-});
+    Route::post('/questions/{question}/vote', 'Api\VoteQuestionController');
+    Route::post('/answers/{answer}/vote', 'Api\VoteAnswerController');
+    });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
